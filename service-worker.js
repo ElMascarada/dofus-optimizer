2671ea560ffc97bccc8591a2a488ef4935905a3e
Slip de Bain <thomas.lauro.tout@gmail.com>
2026-08-23 15:10:15 +0200

    pwa: cache worker solver and normalized snapshot

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,20 @@
-const CACHE = 'dofus-optimizer-v0.2.0';
+const CACHE = 'dofus-optimizer-v0.7.0';
 const APP_SHELL = [
   './', './index.html', './styles.css', './manifest.webmanifest',
+  './data/normalized/dofus-data.json',
   './js/config.js', './js/stats.js', './js/characteristics.js', './js/spells.js',
-  './js/fm.js', './js/sets.js', './js/solver.js', './js/sample-data.js', './js/app.js'
+  './js/fm.js', './js/sets.js', './js/passives.js', './js/build-legality.js',
+  './js/search-space.js', './js/solver.js', './js/solver-worker.js', './js/sample-data.js', './js/app.js'
 ];
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL))));
-self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+});
+
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   event.respondWith(fetch(event.request).then((response) => {
