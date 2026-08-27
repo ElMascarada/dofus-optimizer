be84fb12a298af5e8f9f47546a20789ba0068d45
Slip de Bain <thomas.lauro.tout@gmail.com>
2026-08-27 11:41:07 +0200

    feat: version optimizer search memory

--- js/runtime-meta.js
+++ js/runtime-meta.js
@@ -1,11 +1,18 @@
 const RUNTIME_META = Object.freeze({
-  appVersion: '0.14.2',
-  serviceWorkerCache: 'dofus-optimizer-v0.14.2',
+  appVersion: '0.15.0',
+  serviceWorkerCache: 'dofus-optimizer-v0.15.0',
   searchCache: Object.freeze({
     storageKey: 'dofus-optimizer.search-cache.v1',
     requiredItemsKey: 'dofus-optimizer.required-items.v1',
     epoch: '20260825-required-gear-v1',
     maxEntries: 12
+  }),
+  searchMemory: Object.freeze({
+    rulesVersion: '20260827-rules-v1',
+    searchVersion: '20260827-search-memory-v1',
+    maxRecords: 60,
+    nearbyRecords: 4,
+    maxSeedBuilds: 24
   })
 });
 
